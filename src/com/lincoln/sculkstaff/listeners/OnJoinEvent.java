86f69f5216fd255797434f7ac301a9a4b5617435
LINC0LNmc <lincoln.d.emerson@gmail.com>
2022-06-17 09:23:14 -0400

    20220617: texturepack on join

--- src/com/lincoln/sculkstaff/listeners/OnJoinEvent.java
+++ src/com/lincoln/sculkstaff/listeners/OnJoinEvent.java
@@ -5,6 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import static com.lincoln.sculkstaff.Utils.msgPlayer;
 
 public class OnJoinEvent implements Listener {
     private Main main;
@@ -16,7 +20,10 @@ public class OnJoinEvent implements Listener {
     @EventHandler
     public void joinEvent(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        player.setResourcePack("https://github.com/LINC0LNmc/SculkStaff/blob/master/resource_pack/SculkStaff20220616202930.zip?raw=true");
+        player.setResourcePack("https://github.com/LINC0LNmc/ResourcePacks/blob/master/SculkStaff.zip?raw=true");
+        player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 160, 5));
+        msgPlayer(player, "&3[SculkStaff] You have been given temporary immunity to load your texturepack!");
+
     }
 
 }
